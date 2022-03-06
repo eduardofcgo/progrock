4cd9c374c9ef88fb79381488934d418cca550ad6
Eduardo Goncalves <eduardofcbg@gmail.com>
2022-03-06 23:48:35 +0000

    dev script config env

--- dev.sh
+++ dev.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 cd serve
-find . -name '*.go' -o -name '*.html' | entr -r go run -tags "fts5" main.go ../config/dev.json
+
+export config="../config/dev.json"
+find . -name '*.go' -o -name '*.html' | entr -r go run -tags "fts5" main.go
